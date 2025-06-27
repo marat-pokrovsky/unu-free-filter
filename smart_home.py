32943283ab53de4df71640956cc88820f656b929
marat-pokrovsky <175550684+marat-pokrovsky@users.noreply.github.com>
2025-06-28 00:00:00 +0300

    add file

---
+++ smart_home.py
@@ -0,0 +1,11 @@
+import json
+import os
+import random
+import time
+from datetime import datetime, timedelta
+import threading
+import matplotlib.pyplot as plt
+import numpy as np
+import pytz
+from sklearn.ensemble import IsolationForest
+from sklearn.preprocessing import StandardScaler
